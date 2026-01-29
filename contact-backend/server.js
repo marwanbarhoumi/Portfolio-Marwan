b76cfa6156cc8b391589aff552f640172f310129
marwan2 <marwanbarhou77@gmail.oom.com>
2026-01-29 14:37:49 +0100

    update

--- contact-backend/server.js
+++ contact-backend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ["POST"],
     allowedHeaders: ["Content-Type"]
   })
